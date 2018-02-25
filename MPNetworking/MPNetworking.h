02fac5031e8d100c8fc848063f9380313418c50e
owel <meandmymouse@gmail.com>
2018-02-25 11:44:27 +0300

    Initial Commit

---
+++ MPNetworking/MPNetworking.h
@@ -0,0 +1,19 @@
+//
+//  MPNetworking.h
+//  MPNetworking
+//
+//  Created by owel on 2/25/18.
+//  Copyright © 2018 owel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPNetworking.
+FOUNDATION_EXPORT double MPNetworkingVersionNumber;
+
+//! Project version string for MPNetworking.
+FOUNDATION_EXPORT const unsigned char MPNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPNetworking/PublicHeader.h>
+
+
