c9541439045fb3b9e4442713a72dad5310ab0322
viktart <meandmymouse@gmail.com>
2020-10-18 21:51:15 +0300

    Changed: TODOs were aded, access control modifiers were added to some functions

--- MPNetworking/Networking/Error.swift
+++ MPNetworking/Networking/Error.swift
@@ -9,12 +9,17 @@
 import Foundation
 
 
+// TODO: setup error logging for MPNetworking
+// maybe use custom closure like this one:
+// logError: (Error) -> Void
+// networking.logError = { print($0) }
 public func LogError(_ object : Any) {
     // TODO: add more explicated error logging
     print(object)
 }
 
 
+// TODO: make it MPNetworkingError
 public enum CustomError : Error {
     // Network errors
     case cannotCreateURL(urlString: String)
--- MPNetworking/Networking/NetworkManager.swift
+++ MPNetworking/Networking/NetworkManager.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 open class NetworkManager {
-    
+
+    // TODO: do we really need this typealiases?
     typealias Request = URLRequest
     typealias DataDictionary = [String : Any]
     
@@ -38,7 +39,7 @@ open class NetworkManager {
     
     // MARK: - requests
 
-    func composeRequest(relativeURLString: String, httpMethod: HttpMethod) throws -> Request {
+    internal func composeRequest(relativeURLString: String, httpMethod: HttpMethod) throws -> Request {
         guard let url = URL(string: relativeURLString, relativeTo: serverURL) else {
             let error = CustomError.cannotCreateURL(urlString: relativeURLString)
             LogError(error)
@@ -47,7 +48,7 @@ open class NetworkManager {
         return self.composeRequest(url: url, httpMethod: httpMethod)
     }
     
-    func composeRequest(absoluteURLString: String, httpMethod: HttpMethod) throws -> Request {
+    internal func composeRequest(absoluteURLString: String, httpMethod: HttpMethod) throws -> Request {
         guard let url = URL(string: absoluteURLString) else {
             let error = CustomError.cannotCreateURL(urlString: absoluteURLString)
             LogError(error)
@@ -56,7 +57,7 @@ open class NetworkManager {
         return self.composeRequest(url: url, httpMethod: httpMethod)
     }
     
-    func composeRequest(url: URL, httpMethod: HttpMethod) -> Request {
+    internal func composeRequest(url: URL, httpMethod: HttpMethod) -> Request {
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
         
@@ -66,7 +67,7 @@ open class NetworkManager {
     // MARK: -
     
     // This method can be easily changed in order to tune networking mechanism
-    func performDataRequest( request: Request, callback: @escaping (Data?,URLResponse?,Error?)->Void ) {
+    internal func performDataRequest( request: Request, callback: @escaping (Data?,URLResponse?,Error?)->Void ) {
         let task = self.session.dataTask(with: request) { (data, response, error) in
             callback(data,response,error)
         }
@@ -74,9 +75,11 @@ open class NetworkManager {
     }
 }
 
+
+// TODO: do we really need this as an extension?
 extension NetworkManager {
     /// - parameter callback : Either dictionary or error is equal to nil (not both)
-    func perform( request : Request, callback : @escaping (DataDictionary?, Error?) -> Void ) {
+    internal func perform( request : Request, callback : @escaping (DataDictionary?, Error?) -> Void ) {
         
         self.performDataRequest(request: request) { (data, response, error) in
             guard error == nil else {
